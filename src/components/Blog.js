ce9ac93c2b1edb90c15978cdd147e73380f52ce8
Melody <Melody@Melodys-MacBook-Pro.local>
2017-12-05 15:56:26 -0700

    step 2

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Formated from './subcomponents/Formated';
 
-// import axios
+import axios from 'axios'
 
 class Blog extends Component{
     constructor(){
@@ -12,7 +12,11 @@ class Blog extends Component{
         }
     }
 
-    // insert componentWillMount method
+    componentWillMount(){
+        axios.get(`/api/blog/${this.props.match.params.id}`).then(results => {
+            this.setState({blog: results.data})
+        }).catch(console.log)
+    }
 
     
     render(){
